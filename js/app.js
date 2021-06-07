43f6ee27ba44c950f9688f527c277fb035682123
JaredStephens <jbstephens00@gmail.com>
2021-06-07 12:29:06 -0400

    notification icon

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@ function remove() {
 	alertDiv.parentNode.removeChild(alertDiv);
 	return false;
 }
+
+//REMOVE NOTIFICATION FUNCTION
+
+function notifi() {
+	const bdg = document.querySelector(".badge");
+	bdg.parentNode.removeChild(bdg);
+	return false;
+}
