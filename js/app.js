a3db34b02c61993ed7b8adfd6fbb9ee5aeec4145
JaredStephens <jbstephens00@gmail.com>
2021-06-08 12:59:24 -0400

    chart.js

--- js/app.js
+++ js/app.js
@@ -14,29 +14,96 @@ function notifi() {
 	return false;
 }
 
-// CHARTS
+//////////// CHARTS ///////////////////////////////////
+//////////// CHARTS //////////////////////////////////
+
+///////LINE CHART //////////////////////////////////
 
 // SET-UP
-const labels = ["16-22", "23-19", "30-5", "6-12", "13-19", "20-26"];
+const lineLabels = ["16-22", "23-19", "30-5", "6-12", "13-19", "20-26"];
 
-const data = {
-	labels: labels,
+const lineData = {
+	labels: lineLabels,
 	datasets: [
 		{
-			label: "My First dataset",
-			backgroundColor: "purple",
-			borderColor: "purple",
-			data: [0, 500, 1000, 1500, 2000, 2500],
+			label: "TRAFFIC",
+			backgroundColor: "rgb(116, 119, 191, .3)",
+			borderColor: "rgb(116, 119, 191, .3)",
+			data: [1000, 200, 1000, 400, 1000, 2000],
+			fill: true,
 		},
 	],
 };
 
 // CONFIG
-const config = {
+const lineConfig = {
 	type: "line",
-	data: data,
-	options: {},
+	data: lineData,
 };
 
 // RENDER
-var myChart = new Chart(document.getElementById("myChart"), config);
+var lineChart = new Chart(document.getElementById("lineChart"), lineConfig);
+
+///////BAR CHART //////////////////////////////////
+
+// SETUP
+const barLabels = ["S", "M", "T", "W", "T", "F", "S"];
+const barData = {
+	labels: barLabels,
+	datasets: [
+		{
+			label: "DAILY TRAFFIC",
+			data: [70, 120, 150, 100, 200, 100],
+			backgroundColor: "rgb(116, 119, 191)",
+			borderColor: "rgb(116, 119, 191, .3)",
+			borderWidth: 1,
+		},
+	],
+};
+
+// CONFIG
+const barConfig = {
+	type: "bar",
+	data: barData,
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+};
+
+//RENDER
+var barChart = new Chart(document.getElementById("barChart"), barConfig);
+
+///////PIE CHART //////////////////////////////////
+
+//SETUP
+const doughnutData = {
+	labels: ["Desktop", "Tablet", "Phones"],
+	datasets: [
+		{
+			label: "My First Dataset",
+			data: [300, 50, 100],
+			backgroundColor: [
+				"rgb(116, 119, 191)",
+				"rgb(81, 182, 200)",
+				"rgb(129,201,143)",
+			],
+			hoverOffset: 4,
+		},
+	],
+};
+
+//CONFIG
+const doughnutConfig = {
+	type: "doughnut",
+	data: doughnutData,
+};
+
+//RENDER
+var doughnutChart = new Chart(
+	document.getElementById("doughnutChart"),
+	doughnutConfig
+);
