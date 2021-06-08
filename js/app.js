d4448fb525c7db511e60597978b67e57cbb85e44
JaredStephens <jbstephens00@gmail.com>
2021-06-07 23:50:07 -0400

    setting line chart

--- js/app.js
+++ js/app.js
@@ -13,3 +13,30 @@ function notifi() {
 	bdg.parentNode.removeChild(bdg);
 	return false;
 }
+
+// CHARTS
+
+// SET-UP
+const labels = ["16-22", "23-19", "30-5", "6-12", "13-19", "20-26"];
+
+const data = {
+	labels: labels,
+	datasets: [
+		{
+			label: "My First dataset",
+			backgroundColor: "purple",
+			borderColor: "purple",
+			data: [0, 500, 1000, 1500, 2000, 2500],
+		},
+	],
+};
+
+// CONFIG
+const config = {
+	type: "line",
+	data: data,
+	options: {},
+};
+
+// RENDER
+var myChart = new Chart(document.getElementById("myChart"), config);
